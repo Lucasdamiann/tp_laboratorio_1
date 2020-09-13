7c0482f596a9273dfd5763cc2d254b727241b13b
Lucasdamiann <lucasdamiann@gmail.com>
2020-09-12 22:31:03 -0300

    ultima subida

--- Calculadora.c
+++ Calculadora.c
@@ -2,44 +2,178 @@
  * Calculadora.c
  *
  *  Created on: 5 sep. 2020
- *      Author: Lucasdamiann
+ *      Author: Lucas Bracuto
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include "Calculadora.h"
 
-int suma(float operando1, float operando2) {
-	float resultado;
-	resultado = operando1 + operando2;
-	return resultado;
+int menu() {
+	printf("\n<<BIENVENIDO AL MENU>>\n");
+	setbuf(stdout, NULL);
+	char respuesta = 'r';
+	int comando, bandera1 = 0, bandera2 = 0, bandera3 = 0;
+	float numero1, numero2, calculoSuma, calculoResta, calculoMultip,
+			calculoDiv, calculoFact;
+	do {
+		printf("\n#1-Ingresar 1° operando.\n");
+		printf("#2-Ingresar 2° operando.\n");
+		printf("#3-Realizar calculos.\n");
+		printf("#4-Mostrar resultados.\n");
+		printf("#5-Salir.\n");
+		fflush(stdin);
+		printf("\n>Ingrese una opcion: ");
+		scanf("%d", &comando);
+
+		switch (comando) {
+		case 1:
+			printf("\n>Ingrese el 1° valor:");
+			fflush(stdin);
+			scanf("%f", &numero1);
+			if (numero1 < 0) {
+				printf("¡El valor debe ser positivo!\n");
+			} else {
+				printf("\n> Seleccionó: %.1f <\n", numero1);
+				bandera1 = 1;
+			}
+			break;
+		case 2:
+			if (bandera1 == 1) {
+				printf("\n>Ingrese el 2° valor:");
+				fflush(stdin);
+				scanf("%f", &numero2);
+				if (numero2 < 0) {
+					printf("¡El valor debe ser positivo!\n");
+				} else {
+					printf("\n> Seleccionó: %.1f <\n", numero2);
+					bandera2 = 1;
+				}
+			} else {
+				printf("\n¡Debe ingresar el primer operando!\n");
+			}
+			break;
+		case 3:
+			if (bandera1 == 0) {
+				printf("¡Debe ingresar el primer operando!");
+			} else if (bandera2 == 0) {
+				printf("¡Debe ingresar el segundo operando!");
+			} else if (bandera1 == 1 && bandera2 == 1) {
+				printf("\n¡Calculos realizados!\n");
+				bandera3 = 1;
+			}
+			break;
+		case 4:
+			if (bandera3 == 1) {
+				if (suma(&calculoSuma, numero1, numero2) == 0) {
+					;
+					printf("\nEl resultado de %.1f + %.1f es %.1f: ", numero1,
+							numero2, calculoSuma);
+				}
+				resta(&calculoResta, numero1, numero2);
+				printf("\nEl resultado de %.1f - %.1f es %.1f: ", numero1,
+						numero2, calculoResta);
+				division(&calculoDiv, numero1, numero2);
+				if (numero2 != 0) {
+					printf("\nEl resultado de %.1f / %.1f es %.1f: ", numero1,
+							numero2, calculoDiv);
+				} else {
+					printf("\nNo se puede dividir por 0");
+				}
+				multiplicacion(&calculoMultip, numero1, numero2);
+				printf("\nEl resultado de %.1f * %.1f es %.1f: ", numero1,
+						numero2, calculoMultip);
+				factoreo(&calculoFact, numero1);
+				printf("\nEl factorial de %.1f es: %.1f", numero1, calculoFact);
+				factoreo(&calculoFact, numero2);
+				printf("\nEl factorial de %.1f es: %.1f\n", numero2, calculoFact);
+
+			    printf("\n[Ingrese 'r' para reiniciar]\n"
+			    		"[Cualquier tecla para salir]: ");
+				fflush(stdin);
+				scanf("%c", &respuesta);
+				bandera1 = 0;
+				bandera2 = 0;
+				bandera3 = 0;
+
+			} else {
+				printf("Debe calcular antes de mostrar");
+			}
+
+			break;
+		case 5:
+			printf("\n¡Adios, hasta luego!");
+			respuesta='j';
+			break;
+		default:
+			printf("Ingrese opcion valida\n");
+			break;
+		}
+
+	} while (respuesta == 'r');
+
+	return 0;
 }
 
-int resta(float operando1, float operando2) {
-	float resultado;
-	resultado = operando1 - operando2;
-	return resultado;
-}
-int division(float *pCalculoResultado, float operando1, float operando2) {
-	float resultado;
+int suma(float *pCalculoResultSum, float operando1, float operando2) {
 	int retorno = -1;
-	if (pCalculoResultado != NULL && operando2 != 0) {
-		resultado = (float) operando1 / operando2;
-		*pCalculoResultado = resultado;
+	if (pCalculoResultSum != NULL) {
+		*pCalculoResultSum = operando1 + operando2;
+		retorno = 0;
+	}
+	return retorno;
+}
+
+int resta(float *pCalculoResultRes, float operando1, float operando2) {
+	int retorno =-1;
+	if (pCalculoResultRes != NULL) {
+		*pCalculoResultRes = operando1 - operando2;
+		retorno=0;
+	}
+	return retorno;
+}
+int division(float *pCalculoResultDiv, float operando1, float operando2) {
+
+	int retorno = -1;
+	if (pCalculoResultDiv != NULL && operando2 != 0) {
+		*pCalculoResultDiv = (float) operando1 / operando2;
 		retorno = 0;
 	}
 	return retorno;
 
 }
-int multiplicacion(float operando1, float operando2) {
-	float resultado;
-	resultado = operando1 * operando2;
-	return resultado;
-}
-double factoreoA(float operando1) {
-	float resultado;
-	if (operando1 == 0 || operando1 == 1) {
-		return 1;
+int multiplicacion(float *pCalculoResultMul, float operando1, float operando2) {
+	int retorno=-1;
+	if(pCalculoResultMul != NULL){
+		*pCalculoResultMul = operando1 * operando2;
+		retorno=0;
 	}
-	resultado = operando1 * factoreoA(operando1 - 1);
-	return resultado;
+	return retorno;
+}
+double factoreo(float *pCalculoResultFac, float operando1) {
+	int i;
+	double resultFact = 1;
+	int retorno = -1;
+
+	if (pCalculoResultFac != NULL) {
+		if (operando1 == (int) operando1) {
+			if (operando1 > 0) {
+				for (i = operando1; i > 1; i--) {
+					resultFact *= i;
+				}
+				retorno = 0;
+				*pCalculoResultFac = resultFact;
+
+			} else if (operando1 == 0) {
+				*pCalculoResultFac = 1;
+				retorno = 0;
+			} else {
+				printf("Ingrese un valor positivo\n");
+			}
+		} else {
+			printf("Ingrese un numero entero\n");
+		}
+	}
+	return retorno;
 }
 
--- Calculadora.h
+++ Calculadora.h
@@ -2,45 +2,58 @@
  * Calculadora.h
  *
  *  Created on: 5 sep. 2020
- *      Author: Lucasdamiann
+ *      Author: Lucas Bracuto
  */
 
 #ifndef CALCULADORA_H_
 #define CALCULADORA_H_
 
 #endif /* CALCULADORA_H_ */
-int suma(float operando1, float operando2);
+
+
+
+int menu();
 /*
- * \brief   realiza la suma entre dos valores ingresados por teclado.
- *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
- *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
- *\return   devuelve un float de los valores ingresados por teclado.
+ * \brief    llama a la funcion menu
+  *\return   devuelve 0 para correcto/ distinto de 0 para incorrecto.
  */
-int resta(float operando1, float operando2);
+int suma(float* pCalculoResultSum,float operando1, float operando2);
 /*
- * \brief   realiza la resta entre dos valores ingresados por teclado.
+ *\brief   realiza la suma entre dos valores ingresados por teclado.
+ *\param    valor de puntero de suma.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
- *\return   devuelve float la resta de los valores ingresados por teclado.
+ *\return   devuelve 0 para correcto/ devuelve -1 para incorrecto.
  */
-int division(float *pCalculoResultado, float operando1, float operando2);
+int resta(float* pCalculoResultRes, float operando1, float operando2);
 /*
- * \brief   realiza la division entre dos valores ingresados por teclado.
+ *\brief   realiza la resta entre dos valores ingresados por teclado.
+ *\param    valor de puntero de resta.
+ *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
+ *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
+ *\return   devuelve 0 para correcto/ devuelve -1 para incorrecto.
+ */
+int division(float* pCalculoResultDiv, float operando1, float operando2);
+/*
+ *\brief   realiza la division entre dos valores ingresados por teclado.
+ *\param    valor de puntero de division.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
  *\param    valor ingresado por teclado que puede ser negativo,positivo pero no cero.
- *\return   devuelve float la division de los valores ingresados por teclado.
+ *\return   devuelve 0 para correcto/ devuelve -1 para incorrecto.
  */
-int multiplicacion(float operando1, float operando2);
+int multiplicacion(float* pCalculoResultMul,float operando1, float operando2);
 /*
- * \brief   realiza la multiplicacion entre dos valores ingresados por teclado.
+ *\brief   realiza la multiplicacion entre dos valores ingresados por teclado.
+ *\param    valor de puntero de multiplicacion.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
- *\return   devuelve float la multiplicacion de los valores ingresados por teclado.
+ *\return   devuelve 0 para correcto/ devuelve -1 para incorrecto.
  */
-double factoreoA(float operando1);
+double factoreo(float* pCalculoResultFac, float operando1) ;
 /*
- * \brief   realiza la factorizacion de un valor ingresado por teclado
+ *\brief   realiza la factorizacion de un valor ingresado por teclado.
+ *\param    valor de puntero de factorial.
  *\param    valor ingresado por teclado que puede ser negativo,positivo o cero.
- *\return   devuelve float la factorizacion del valor ingresado por teclado
+ *\return   devuelve 0 para correcto/ devuelve -1 para incorrecto.
  */
 
